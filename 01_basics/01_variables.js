05e309b848cf7ff2321a982df7a65db59f8b3121
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-19 18:46:03 +0000

    conversion of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14453
+let accountEmail = "tarun100@gmail.com"
+var accountPassword = "1234"
+accountCity = "Bhopal"
+let accoutState
+
+// accountId = 2                                             changing const not allowed
+
+accountEmail = "adjaiehfdl"                                  //  let variables can be changed
+accountPassword = "374394384"
+accountCity = "chennai"
+
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accoutState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";                                           // treat all js code as newer version
+
+//alert(3+3)                                               we are using nodejs not browser
+
+let name = "tarun"
+let age = 25
+let isLoggedIn = false
+
+
+/* 
+number => 2 to power 53
+string => ""
+boolean => true/false
+null => standalone value 
+undefined => when value is not initialized
+symbol => unique
+object 
+*/
+
+
+console.log(typeof undefined)   // undefined
+console.log(typeof null)       // object
