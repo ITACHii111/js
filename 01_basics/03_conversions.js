108da664091c1724bedf6698268612e2a3452ec9
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-20 04:00:11 +0000

    changes

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -27,5 +27,7 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof someNumber)
+//console.log(stringNumber)
+//console.log(typeof someNumber)
+
+// operations
