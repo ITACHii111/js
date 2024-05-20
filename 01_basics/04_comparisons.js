279ab1d02b12edcdf185e4edd6fc272bb9e19841
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-20 04:26:15 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,16 @@
+// console.log(2 == 1)
+// console.log(2 >= 1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)               // null is converted 0 false
+// console.log(null == 0)
+// console.log(null >= 0)              // null is converted 0 true
+
+// comparison should be done on same data type
+
+// === not just check value but also the data type
+
+console.log("2" === 2);
