fe195a42c6051133defba8a05760fe206eef8d89
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-20 04:15:46 +0000

    operations

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -31,3 +31,21 @@ let stringNumber = String(someNumber)
 //console.log(typeof someNumber)
 
 // operations
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "tarun"
+let str2 = " chandel"
+
+let str3 = str1+str2
+//console.log(str3)
+
+console.log("1" + 2 + 2)
+
+console.log(1 + "2")
+
+console.log(1 + 2 + "2")
+
+console.log(1 + "abs")
