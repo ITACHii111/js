5e2c7121810fdac07d0f36462a53fb5a9b557b45
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-21 05:17:23 +0000

    math&num

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400               // defining a number
+
+const balance = new Number(100)
+// console.log(score)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+ 
+// +++++++++++++++ Maths ++++++++++++++++++++
+
+// console.log(Math)
+
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+
+console.log(Math.random()*10 +1)        // random always gives value between 0's and 1's and always random
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
