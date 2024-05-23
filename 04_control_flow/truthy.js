cb378469b3db4011bc78fe04cb9bc512edacbe5e
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-23 16:28:30 +0000

    truth_value

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,15 @@
+const userEmail = []
+
+if(userEmail){
+    console.log("got user email");
+} else {
+    console.log("don't have user email");
+}
+
+/* 
+falsy value 
+    false, 0, -0, BigInt 0n, empty string, null, undefined, NaN
+
+truthy value
+    "0", 'false', " ", [], {}, fuction(){}
+*/
