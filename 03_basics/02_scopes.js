bf4f2da8eeb7a35be00a019e2de345238b4daadb
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-23 14:13:34 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,26 @@
+
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// +++++++++++++++++ interesting +++++++++
+
+
+console.log(addone(5))
+function addone(num) {
+    return num+1
+}
+
+console.log(addTwo(5))
+
+const addTwo = function(num){
+    return num+2
+}
