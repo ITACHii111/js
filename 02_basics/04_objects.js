6475c838605016e93601fade7d3547bc958a8234
ITACHii111 <113909853+ITACHii111@users.noreply.github.com>
2024-05-23 06:47:38 +0000

    jsonformat

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,7 +24,7 @@ const obj2 = {3: "a", 4: "b"}
 // const obj3 = Object.assign({}, obj1, obj2)
 
 const obj3 = {...obj1, ...obj2}        // spread operator
-console.log(obj3)
+// console.log(obj3)
 
 
 const users = [
@@ -38,4 +38,39 @@ const users = [
     }
 ]
 
-console.log(users[1].email)
+// console.log(users[1].email)
+
+// destructuring in objects
+
+
+const course = {
+    coursename: "jsInHindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+// const navbar = ({company}) => {
+
+// }                desturcture in react example
+
+// navbar(company = "hitesh")
+
+
+// concepts of API (apna kaam kisi aur ke sar par dalna)
+
+// {
+//     "name": "tarun",
+//     "courseName": "js in Hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
